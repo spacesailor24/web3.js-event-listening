574878cf678c6174d48d91600ade7d3ffd11c050
Wyatt Barnes <me@wyatt.email>
2023-06-22 22:31:22 -1000

    Refactor USDC_TRANSFERRED_FUNCTION_SIGNATURE to TRANSFER_EVENT_TOPIC

--- src/usdc-transfer-listener.ts
+++ src/usdc-transfer-listener.ts
@@ -1,16 +1,17 @@
-import { Web3 } from "web3";
+import { Web3, utils } from "web3";
 
 (async () => {
   const USDC_VERIFIED_ADDRESS = "0x6b175474e89094c44da98b954eedeac495271d0f";
-  const USDC_TRANSFERRED_FUNCTION_SIGNATURE =
-    "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef";
+  const TRANSFER_EVENT_TOPIC = utils.keccak256(
+    "Transfer(address,address,uint256)"
+  );
   const NUMBER_OF_BLOCKS = BigInt(10);
 
   const web3 = new Web3("http://localhost:1234");
 
   const logs = await web3.eth.getPastLogs({
     address: USDC_VERIFIED_ADDRESS,
-    topics: [USDC_TRANSFERRED_FUNCTION_SIGNATURE],
+    topics: [TRANSFER_EVENT_TOPIC],
     // This PR will allows us to pass a BigInt instead of having to format it as a hex string
     // https://github.com/web3/web3.js/pull/6219
     fromBlock: `0x${(
