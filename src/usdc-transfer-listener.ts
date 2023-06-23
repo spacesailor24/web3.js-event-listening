76cc16f6c9e22532b389e881266411e136e9f877
Wyatt Barnes <me@wyatt.email>
2023-06-22 22:09:39 -1000

    Replace magic number with NUMBER_OF_BLOCK constant

--- src/usdc-transfer-listener.ts
+++ src/usdc-transfer-listener.ts
@@ -4,6 +4,7 @@ import { Web3 } from "web3";
   const USDC_VERIFIED_ADDRESS = "0x6b175474e89094c44da98b954eedeac495271d0f";
   const USDC_TRANSFERRED_FUNCTION_SIGNATURE =
     "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef";
+  const NUMBER_OF_BLOCKS = BigInt(10);
 
   const web3 = new Web3("http://localhost:1234");
 
@@ -12,9 +13,9 @@ import { Web3 } from "web3";
     topics: [USDC_TRANSFERRED_FUNCTION_SIGNATURE],
     // This PR will allows us to pass a BigInt instead of having to format it as a hex string
     // https://github.com/web3/web3.js/pull/6219
-    fromBlock: `0x${((await web3.eth.getBlockNumber()) - BigInt(10)).toString(
-      16
-    )}`,
+    fromBlock: `0x${(
+      (await web3.eth.getBlockNumber()) - NUMBER_OF_BLOCKS
+    ).toString(16)}`,
   });
 
   console.log(logs);
