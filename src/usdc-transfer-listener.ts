7e5fce5927ef265bac572d605da50344e931e7fb
Wyatt Barnes <me@wyatt.email>
2023-06-22 21:51:48 -1000

    Init usdc-transfer-listener

---
+++ src/usdc-transfer-listener.ts
@@ -0,0 +1,21 @@
+import { Web3 } from "web3";
+
+(async () => {
+  const USDC_VERIFIED_ADDRESS = "0x6b175474e89094c44da98b954eedeac495271d0f";
+  const USDC_TRANSFERRED_FUNCTION_SIGNATURE =
+    "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef";
+
+  const web3 = new Web3("http://localhost:1234");
+
+  const logs = await web3.eth.getPastLogs({
+    address: USDC_VERIFIED_ADDRESS,
+    topics: [USDC_TRANSFERRED_FUNCTION_SIGNATURE],
+    // This PR will allows us to pass a BigInt instead of having to format it as a hex string
+    // https://github.com/web3/web3.js/pull/6219
+    fromBlock: `0x${((await web3.eth.getBlockNumber()) - BigInt(10)).toString(
+      16
+    )}`,
+  });
+
+  console.log(logs);
+})();
